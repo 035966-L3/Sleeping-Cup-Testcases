a5a0750c68dfcf99ab820f0eac13f0db7bbbd02f
035966-L3 <139025318+035966-L3@users.noreply.github.com>
2025-08-14 16:20:55 +0800

    Add hacks on #5/D to resolve Issue #2

---
+++ #5/D/gen_6.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	freopen("6.in", "w", stdout);
+	puts("1000");
+	printf("1");
+	for(int i = 1; i <= 1000; i++)
+		printf("0");
+	puts("");
+	for (int i = 1; i <= 999; i++)
+		printf("%d\n", i);
+	return 0;
+}
